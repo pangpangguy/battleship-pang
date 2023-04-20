8e4d1d86a84b5701cb14c31d937f2b4fa752492d
Pang <ryanfx1998@gmail.com>
2023-04-20 12:57:44 +0800

    First commit for project setup (via vite)

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Battleship App</h1>
+      <div className="card">
+        <p>Example text</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;

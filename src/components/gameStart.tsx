73e559fbe1226f3eeecdc82b6fa65582dd64d399
Pang <ryanfx1998@gmail.com>
2023-05-19 16:43:57 +0800

    feat : Implemented logic for sinking ship

--- src/components/gameStart.tsx
+++ src/components/gameStart.tsx
@@ -3,6 +3,7 @@ import { GameStartCellInfo, CellState } from "../common/types";
 import Board from "./board";
 import "./gamestart.css";
 import { shipList } from "../common/constants";
+import { cellHasShip } from "../common/utils";
 
 interface GameStartProps {
   playerBoard: GameStartCellInfo[][];
@@ -19,6 +20,7 @@ export default function GameStart({
   handleUpdatePlayerBoard,
   handleRestartGame,
 }: GameStartProps) {
+  const [opponentShipsRemaining, setOpponentShipsRemaining] = useState(new Map<string, number>(initializeScoreMap()));
   const [discoverOutcomeMessage, setDiscoverOutcomeMessage] = useState<string>("");
   const timeoutId = useRef<ReturnType<typeof setTimeout> | null>(null);
 
@@ -26,12 +28,24 @@ export default function GameStart({
     return shipList.reduce((map, ship) => map.set(ship.acronym, ship.size), new Map());
   }
 
-  function getCellsToUpdate(targetCell: GameStartCellInfo): GameStartCellInfo[] {
+  function getCellsToUpdate(
+    targetCell: GameStartCellInfo,
+    shipsRemaining: Map<string, number>,
+    setShipsRemaining: (value: (prevState: Map<string, number>) => Map<string, number>) => void
+  ): GameStartCellInfo[] {
     const cellsToUpdate: GameStartCellInfo[] = [];
 
     // If Sunk
-    if (false) {
-      //TODO: Handle changing state from Hit to Sunk
+    if (
+      cellHasShip(targetCell) &&
+      checkShipIsSunkAndUpdateShipsRemaining(targetCell.shipId, shipsRemaining, setShipsRemaining)
+    ) {
+      const cellsChangeToSunk = opponentBoard
+        .flat()
+        .filter((cell) => cellHasShip(cell) && cell.shipId === targetCell.shipId)
+        .map((cell) => ({ ...cell, cellState: CellState.Sunk, isDiscovered: true }));
+      console.log(cellsChangeToSunk);
+      cellsToUpdate.push(...cellsChangeToSunk);
     } else {
       // Hit or Miss
       cellsToUpdate.push({ ...targetCell, isDiscovered: true });
@@ -48,13 +62,43 @@ export default function GameStart({
       return;
     }
 
-    const cellsToUpdate = getCellsToUpdate(targetCell);
+    const cellsToUpdate = getCellsToUpdate(targetCell, opponentShipsRemaining, setOpponentShipsRemaining);
     showDiscoverOutcomeMessage(cellsToUpdate[0].cellState);
     handleUpdateOpponentBoard(cellsToUpdate);
   }
 
   function discoverAICell(id: string) {
     //TODO: Implement AI
+  }
+
+  // Check if the ship that is hit will be sunk
+  // If sunk, remove from map and return true; otherwise, update the ship parts remaining and return false
+  function checkShipIsSunkAndUpdateShipsRemaining(
+    shipId: string,
+    shipsRemaining: Map<string, number>,
+    setShipsRemaining: (value: (prevState: Map<string, number>) => Map<string, number>) => void
+  ): boolean {
+    const targetShipParts = shipsRemaining.get(shipId);
+
+    //Shouldn't happen
+    if (!targetShipParts) {
+      throw new Error(`Unexpected error: ship with ID ${shipId} not found in map.`);
+    }
+
+    //Check if sunk and remove from map
+    if (targetShipParts - 1 === 0) {
+      setShipsRemaining((prev) => {
+        const updatedMap = new Map(prev);
+        updatedMap.delete(shipId);
+        return updatedMap;
+      });
+
+      return true;
+    }
+
+    //Else update the map if is only Hit.
+    setShipsRemaining((prev) => new Map(prev).set(shipId, targetShipParts - 1));
+    return false;
   }
 
   function showDiscoverOutcomeMessage(state: CellState) {

d1219f9a4fc5e70a9c62dfd3641d0e6aed5f67de
Pang <ryanfx1998@gmail.com>
2023-05-31 12:33:07 +0800

    feat : responsive leaderboard

--- src/components/leaderboard.tsx
+++ src/components/leaderboard.tsx
@@ -59,9 +59,9 @@ export default function MainPage({ handleReturnMainPage }: LeaderboardProps) {
       {leaderboard.map((entry, index) => {
         return (
           <div className={`board-entry ${getMedalClass(index + 1)}`} key={index}>
-            <h2>{getPlacementIndex(index + 1)}</h2>
-            <h2>{entry ? entry.name : "---"}</h2>
-            <h2>{entry ? entry.score : "---"}</h2>
+            <h3>{getPlacementIndex(index + 1)}</h3>
+            <h3>{entry ? entry.name : "---"}</h3>
+            <h3>{entry ? entry.score : "---"}</h3>
           </div>
         );
       })}

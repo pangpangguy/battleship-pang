5c12308cb76346726649b009cf63499f157b9417
Pang <ryanfx1998@gmail.com>
2023-06-01 13:27:54 +0800

    feat : Add animation for main page and input validation

--- src/components/mainpage.tsx
+++ src/components/mainpage.tsx
@@ -1,3 +1,4 @@
+import classNames from "classnames";
 import "./mainpage.css";
 
 interface MainPageProps {
@@ -16,8 +17,28 @@ export default function MainPage({
   return (
     <div className="mainpage-container animate__animated animate__fadeIn">
       <h1 className="title">Super Cool Battleship Game</h1>
-      <input type="text" className="name-input" placeholder="Enter Your Name" onChange={handleNameInputChange} />
-      <button onClick={handleEnterPregame} className="main-page-btn" disabled={playerName.length <= 0}>
+      <input
+        type="text"
+        maxLength={11}
+        className={classNames("name-input", {
+          "input-error": playerName.length > 10,
+        })}
+        placeholder="Enter Your Name"
+        onChange={handleNameInputChange}
+      />
+      <h2></h2>
+      <h2
+        className="input-error-message"
+        style={{ visibility: playerName.length > 10 ? "visible" : "hidden" }}
+        key={playerName}
+      >
+        Please enter no more than 10 characters.
+      </h2>
+      <button
+        onClick={handleEnterPregame}
+        className="main-page-btn"
+        disabled={!playerName.length || playerName.length > 10}
+      >
         Start Game
       </button>
 
